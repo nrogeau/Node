1fa22f36fc1443afb2d029d64cca33c19ea1242a
nrogeau <57152105+nrogeau@users.noreply.github.com>
2025-05-06 18:15:49 +0900

    To Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Node/', // Set this to your GitHub repo name
   plugins: [react()],
-})
+  base: '/Node/', // Set the base path for GitHub Pages deployment
+});
+
